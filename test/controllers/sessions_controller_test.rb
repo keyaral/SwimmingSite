69ddbde2e1bbec9eaf43ded4ee2a6f413c8fbe6d
keyaral <maz-firebabe@hotmail.com>
2014-10-24 12:02:28 +1300

    Finish user edit, index update and destroy

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,5 +5,16 @@ class SessionsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
+def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+      redirect_back_or user
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 
 end
